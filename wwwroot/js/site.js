775f0662662e0d7216ed3491e4d0347ab7d78c55
gustavo <gustavo.maia@b2b.com.pt>
2020-04-06 15:11:37 +0100

    Add Data Table and Font Awesome Icons

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,10 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+
+//cdn-table
+
+$(document).ready(function () {
+    $('#tblData').DataTable();
+});
